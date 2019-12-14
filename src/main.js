d56cb74418c04db6d3600f55a9690aeb685b0ae5
liyang14 <liyang14@kuaishou.com>
2019-12-14 20:25:47 +0800

    demo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// debug
+Vue.config.devtools = true
+
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo is this.</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo }
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
